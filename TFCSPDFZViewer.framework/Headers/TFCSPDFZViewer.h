273ccf24532d66029d532b9b215f8b04608c3f59
Jeff Lee <jefflee@Jeffs-MacBook-Pro.local>
2018-10-11 11:59:49 +0800

    swift

--- TFCSPDFZViewer.framework/Headers/TFCSPDFZViewer.h
+++ TFCSPDFZViewer.framework/Headers/TFCSPDFZViewer.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double TFCSPDFZViewerVersionNumber;
 FOUNDATION_EXPORT const unsigned char TFCSPDFZViewerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TFCSPDFZViewer/PublicHeader.h>
-
-

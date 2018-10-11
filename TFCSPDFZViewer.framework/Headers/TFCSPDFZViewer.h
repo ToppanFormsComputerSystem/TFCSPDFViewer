1872f41e2eb276a0c6121d986a07e1202721b77c
Jeff Lee <jefflee@Jeffs-MacBook-Pro.local>
2018-10-11 11:43:41 +0800

    Update arm

---
+++ TFCSPDFZViewer.framework/Headers/TFCSPDFZViewer.h
@@ -0,0 +1,19 @@
+//
+//  TFCSPDFZViewer.h
+//  TFCSPDFZViewer
+//
+//  Created by Jeff Lee on 11/10/2018.
+//  Copyright © 2018 TFCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TFCSPDFZViewer.
+FOUNDATION_EXPORT double TFCSPDFZViewerVersionNumber;
+
+//! Project version string for TFCSPDFZViewer.
+FOUNDATION_EXPORT const unsigned char TFCSPDFZViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFCSPDFZViewer/PublicHeader.h>
+
+
